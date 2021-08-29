421b7b6695c837918c3bdf75875a0ea5602d8b0e
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-08-28 22:11:09 -0400

    rework quiz functionality, add base timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,192 +1,130 @@
-// Functions
+// setting variables
+
+var pos = 0;
+var correct = 0;
+var test, test_status, question, choice, choices, choiceA, choiceB, choiceC, choiceD;
+
+
+// timer functions
+
+var sec = 90;
+var time = setInterval(quizTimer, 1000);
+
+
+function quizTimer() {
 
 
 
-
-//runquiz function
-function runQuiz() {
-    showSlide();
-    //variable to store the HTML output
-    const output = [];
-
-    quizQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            //variable to store the list of possible answers
-            const answers = [];
-
-            //and for each available answer
-            for(letter in currentQuestion.answers){
-
-                answers.push(
-                    `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}"></input>
-                            ${letter} :
-                            ${currentQuestion.answers[letter]}
-                        
-                    </label>`
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                `<div class="slide">
-                <div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>
-                </div>`
-            );
-        }
-    );
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-};
-
-//showresults function
-function showResults() {
-
-    //gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-
-    // keep track of user's answers
-    let numCorrect = 0;
-
-    // for each question
-    quizQuestions.forEach( (currentQuestion, questionNumber) => {
-
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-        //if answer is correct
-        if (userAnswer === currentQuestion.correctAnswer) {
-
-            //add to the nunber of correct answers
-            numCorrect++;
-
-            // color the answers green
-            answerContainers[questionNumber].style.color = 'lightgreen';
-        }
+    document.getElementById('timer').innerHTML = sec + " seconds left!";
+    sec--;
+    
+    if (sec === -1) {
+        clearInterval(time);
         
-        // if answer is incorrect, blank
-        else {
-            // color the answers red
-            answerContainers[questionNumber].style.color = 'red';
-        }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
-};
-
-
-//showSlide function
-function showSlide(n) {
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-    if (currentSlide === 0) {
-        previousButton.style.display = 'none';
-    }
-    else {
-        previousButton.style.display = 'inline-block';
-    }
-    if (currentSlide === slides.length-1) {
-        nextButton.style.display = 'none';
-        submitButtonEl.style.display = 'inline-block';
-    }
-    else {
-        nextButton.style.display = 'inline-block';
-        submitButtonEl.style.display = 'none';
     }
 };
 
+// end timer functions
 
-//Previous/Next slide buttons functions
-function showNextSlide() {
-    showSlide(currentSlide + 1);
-};
+//quiz questions array
 
-function showPreviousSlide() {
-    showSlide(currentSlide - 1);
-};
-
-
-
-
-// variables :)
-
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButtonEl = document.getElementById("submit");
-const startButtonEl = document.getElementById("start");
-
-const quizQuestions = [
+var quizQuestions = [
     {
         question: "To acquire an element by its ID in an entire document, you would use _________",
-        answers: {
             a: "document.getElementById('element')",
             b: "document.getElementsByClassName('element')",
             c: "document.getElementsByTagName('element')",
-            d: "document.getElementById=element"
-            
+            d: "document.getElementById=element",
+            answer: "A"
         },
-        correctAnswer: "a"
-    },
     {
         question: "You can declare a variable using 'var.' What other option is a more specific way to declare a variable?",
-        answers: {
             a: "constVar",
             b: "letVar",
             c: "renewVar",
-            d: "let"
-        },
-        correctAnswer: "d"
+            d: "let",
+            answer: "D"
     },
     {
         question: "To declare a variable that will never change, use...",
-        answers: {
             a: "hardVar",
             b: "softVar",
             c: "const",
-            d: "topConst"
-        },
-        correctAnswer: "c"
+            d: "topConst",
+            answer: "C"
     },
     {
         question: "How can you use inline JS?",
-        answers: {
             a: "Trick question, JS must always be linked to an external sheet.",
             b: "add a script tag in the HTML",
             c: "add a javaScript tag in the HTML",
-            d: "just type it in there man who cares"
-        },
-        correctAnswer: "b"
+            d: "just type it in there man who cares",
+            answer: "B"
     }
-
 ];
 
+function get(x) {
+    return document.getElementById(x);
+}
 
-// Testing functions
+function displayQuestion() {
+
+    test = get("test");
+    if (pos >= quizQuestions.length) {
+        test.innerHTML = "<h2>You got " + correct + " of " + quizQuestions.length + " questions correct.</h2>";
+        get("test_status").innerHTML = "Test completed";
+
+        //reset variable to allow restart
+        pos = 0;
+        correct = 0;
+
+        //stop displayQuestion function when test is complete
+        return false;
+    }
+
+    get("test_status").innerHTML = "Question " + (pos+1) + " of " + quizQuestions.length;
+
+    quizQuestion = quizQuestions[pos].question;
+    choiceA = quizQuestions[pos].a;
+    choiceB = quizQuestions[pos].b;
+    choiceC = quizQuestions[pos].c;
+    choiceD = quizQuestions[pos].d;
+
+    // display the question
+    test.innerHTML = "<h3>" + quizQuestion + "</h3>";
+
+    //display the answer options
+    //the += appends to the data we started on the line above
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> " + choiceA + "</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> " + choiceB + "</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> " + choiceC + "</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='D'> " + choiceD + "</label><br>";
+    test.innerHTML += "<button id='submit' onclick='checkAnswer()'>Submit Answer</button>";
 
 
-//Pagination
 
-const previousButton = document.getElementById("previous");
-const nextButton = document.getElementById("next");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
+}
 
+function checkAnswer() {
+    // use getElementsByName because we have an array which it will loop through
+    choices = document.getElementsByName("choices");
+    for (var i=0; i < choices.length; i++) {
+        if (choices[i].checked) {
+            choice = choices[i].value;
+        }
+    }
 
-//Testing show slide
-runQuiz();
-showSlide(currentSlide);
+    // check if answer matches correct choice
+    if (choice === quizQuestions[pos].answer) {
+        correct++;
+    } else if (choice !== quizQuestions[pos].answer) {
+        sec = sec - 10;
+    };
+    
+    pos++;
+    displayQuestion();
+}
 
-
-// Event listeners
-startButtonEl.addEventListener("click", runQuiz);
-startButtonEl.addEventListener("click", showSlide);
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
-submitButtonEl.addEventListener("click", showResults);
+document.getElementById("start").addEventListener("click", displayQuestion);
+document.getElementById("start").addEventListener("click", quizTimer);
+document.getElementById("submit").addEventListener("click", resetTimer);
